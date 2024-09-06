d5f8e7ed3ba0225ba4fa5f60611f170695c45249
Huroad <cabin9999@naver.com>
2024-09-06 15:34:01 +0900

    feat:setting

---
+++ src/main/java/com/logether/LogetherApplication.java
@@ -0,0 +1,13 @@
+package com.logether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogetherApplication.class, args);
+    }
+
+}
